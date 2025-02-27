621976788fa640855f8c2ebd11d47c6734584fea
Shams Ben Mefteh <shams.ben.mefteh5@gmail.com>
2025-02-27 13:55:38 +0100

    fixed pipeline, accuracy improved to 90

--- app.py
+++ app.py
@@ -1,8 +1,51 @@
 import streamlit as st
 import pickle
 import time
+import re
+import string
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from sklearn.base import BaseEstimator, TransformerMixin
+
+
+# Initialize STOPWORDS
+STOPWORDS = set(stopwords.words('english'))
+
+def preprocess_text(text, return_tokens=False):
+    # Convert to lowercase
+    text = text.lower()
+    
+    # Remove numbers
+    text = re.sub(r'\d+', " ", text)
+    
+    # Remove punctuation
+    text = text.translate(str.maketrans('', '', string.punctuation))
+    
+    # Tokenization
+    tokens = word_tokenize(text)
+    
+    # Remove stopwords
+    tokens = [word for word in tokens if word not in STOPWORDS]
+    
+    # Remove extra white spaces (single-character tokens)
+    tokens = [token for token in tokens if len(token) > 1]
+    
+    if return_tokens:
+        return tokens
+    else:
+        return " ".join(tokens)
+
+class TextPreprocessor(BaseEstimator, TransformerMixin):
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X):
+        return [preprocess_text(text) for text in X]
+
 def to_dense(X):
     return X.toarray()
+
 model = pickle.load(open('review_sentiment.pkl', 'rb'))
 st.title("Review Sentiment Analysis")
 
